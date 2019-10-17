e54885a25d4e8101a7d9085172b1cd27dc1ce6d8
abarne <celius377@gmail.com>
2019-10-17 14:57:43 -0500

    edit-movie route added and basic form added

---
+++ client/src/Movies/UpdateMovie.js
@@ -0,0 +1,67 @@
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+
+const initialItem = {
+	title: '',
+	director: '',
+	metascore: 0,
+	stars: []
+};
+
+const UpdateMovie = (props) => {
+	const [ movie, setMovie ] = useState(initialItem);
+
+	useEffect(() => {
+		const itemToEdit = {};
+		axios
+			.get(`http://localhost:5000/api/movies/${props.match.params.id}`)
+			.then((response) => {
+				console.log(response);
+				setMovie(response.data);
+			})
+			.catch((err) => console.log(err.response));
+	}, []);
+
+	const changeHandler = (e) => {
+		e.persist();
+		let value = e.target.value;
+		if (e.target.name === 'metascore') {
+			value = parseInt(value, 10);
+		}
+		setMovie({
+			...movie,
+			[e.target.name]: value
+		});
+	};
+
+	const handleSubmit = (e) => {
+		e.preventDefault();
+		console.log('You made it to submit! here is the updated movie', movie);
+	};
+
+	return (
+		<div>
+			<h2>Update Movie</h2>
+			<form onSubmit={handleSubmit}>
+				<input type="text" name="title" onChange={changeHandler} placeholder="title" value={movie.title} />
+				<input
+					type="text"
+					name="director"
+					onChange={changeHandler}
+					placeholder="director"
+					value={movie.director}
+				/>
+				<input
+					type="number"
+					name="metascore"
+					onChange={changeHandler}
+					placeholder="metascore"
+					value={movie.metascore}
+				/>
+				<button>Update Movie</button>
+			</form>
+		</div>
+	);
+};
+
+export default UpdateMovie;
